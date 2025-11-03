d23bb7be6bfd0232b08b7ff93641e9a79657612d
NikolaSirakov3 <nikola@melterrea.com>
2025-11-03 20:21:10 +0700

    Add new logo images and font file; update App component structure with Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <header className="fixed border-b border-b-zinc-300 bg-white/50 backdrop-blur-lg min-w-full py-6 lg:py-7 px-4 sm:px-8 max-w-7xl overflow-x-clip">
+      <div className="container mx-auto flex items-center justify-between">
+        {/* Logo */}
+        <div className=""></div>
+        {/* Desktop Nav */}
+        <nav className=""></nav>
+        {/* Mobile Menu Icon */}
+        <div className=""></div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
+

8ddcde526dc18bcef633271be41a9e98a0c1eb79
NikolaSirakov3 <nikola@melterrea.com>
2025-11-04 17:44:22 +0700

    Update Header component to include text logo; replace empty logo div with image element. Update logo images in the public directory.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,9 @@ const Header = () => {
     <header className="fixed border-b border-b-zinc-300 bg-white/50 backdrop-blur-lg min-w-full py-6 lg:py-7 px-4 sm:px-8 max-w-7xl overflow-x-clip">
       <div className="container mx-auto flex items-center justify-between">
         {/* Logo */}
-        <div className=""></div>
+        <div className="flex items-center">
+          <img src="/text-logo.png" alt="FurniLab" className="h-8 lg:h-10" />
+        </div>
         {/* Desktop Nav */}
         <nav className="hidden lg:flex items-center space-x-8">
           <NavBtn>Home</NavBtn>

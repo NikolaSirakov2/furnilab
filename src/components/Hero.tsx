bde81154c0aed0ab3d46b1ecc8a7c1755b7315ea
NikolaSirakov3 <nikola@melterrea.com>
2025-11-06 12:34:31 +0700

    Add sofa image to public directory and adjust Hero component layout for improved responsiveness

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,19 +3,19 @@ import HeroImage from "./HeroImage";
 
 const Hero = () => {
   return (
-    <section className="max-w-7xl mt-30 mx-auto p-4 sm:p-8 mb-20">
+    <section className="max-w-[1600px] mt-30 mx-auto p-4 sm:p-8 mb-20">
       <div className="flex relative flex-col lg:flex-row lg:space-x-12 overflow-x-clip">
         {/* Left Content */}
         <div className="lg:w-1/2">
           {/* Main Title: Large Bold */}
           <h1 className="text-7xl sm:text-8xl font-extrabold leading-none mb-6">
-            <span className="block impact lg:text-[250px]">FINE</span>
-            <span className="block impact lg:text-[250px] tracking-tight lg:mt-10">
+            <span className="block impact lg:text-[240px]">FINE</span>
+            <span className="block impact lg:text-[230px] tracking-tight lg:mt-10">
               FURNISHINGS
             </span>
           </h1>
           {/* SubText */}
-          <div className="text-base lg:text-lg max-w-md lg:w-full mb-8 py-2 text-zinc-600">
+          <div className="text-base lg:text-lg max-w-md lg:w-full mb-8 py-2 pl-2 text-zinc-600">
             Choosing the right furniture for your home online will add elegance
             and functionality to your space. We offer a curated collection of
             fine furnishing.
